c07fa0f801cc2e2717c0bb125a668155653b1fce
Joel Santos <i3975@rn.senac.local>
2025-07-15 17:22:32 -0300

    fix: Correção da configuração do jest na Pasta Aula05

--- Aula05/src/modules/produto/controllers/produtoController.js
+++ Aula05/src/modules/produto/controllers/produtoController.js
@@ -2,9 +2,22 @@ const produtoModel = require('../models/produtoModel');
 
 class ProdutoController {
 
-    static async criar(dados){
-        //const {nome, preco, estoque} = req.body;
-        const produto = produtoModel.create(dados);
+    static async criar(req, res){
+        try{
+            const {nome, preco, estoque} = req.body;
+    
+                if (nome && preco && estoque) {
+                    const produto = produtoModel.create({nome, preco, estoque});
+                    res.status(201).json({mensagem: 'Aluno cadastrado com sucesso', aluno })
+                } else {
+                    res.status(400).json({mensagem: 'Todos os campos devem ser preenchidos' })
+                }
+        }catch(error){
+            res.status(500).json({ erro: error.message });
+        }
+        
+
+        
         return produto
     }
 
--- Aula05/src/modules/produto/tests/produtoController.test.js
+++ Aula05/src/modules/produto/tests/produtoController.test.js
@@ -1,20 +1,34 @@
 const Produto = require('../models/produtoModel');
 const ProdutoControler = require('../controllers/produtoController');
-const {sequelize} = require('../../../config/configDB');
+const { sequelize } = require('../../../config/configDB');
 
-beforeAll( async () =>{
-    await sequelize.sync({force: true})
+beforeAll(async () => {
+    await sequelize.sync({ force: true })
 })
-afterAll(async ()=>{
+afterAll(async () => {
     await sequelize.close();
 })
-describe('Testes do Produto Controller', ()=>{
-    test('Deve criar um produto no banco de dados', async ()=>{
+describe('Testes do Produto Controller', () => {
+    test('Deve criar um produto no banco de dados', async () => {
         //Aqui estamos realizando uma requisição para a criação de produto
-        const produto = await ProdutoControler.criar({nome: 'Feijão', preco: 4.90, estoque:500});
+        const req = { body: { nome: 'Feijão', preco: 4.90, estoque: 500 } }
+        const produto = await ProdutoControler.criar(req);
         // Analisamos a const produto para verificar os dados retornados
         expect(produto).toHaveProperty('cod_prod');
         expect(produto.nome).toBe('Feijão');
-       expect(produto.preco).toBe(4.90);
+        expect(produto.preco).toBe(4.90);
     });
+
+    test('Deve retornar um erro caso não seja passado parametros no body', async () => {
+        //Aqui estamos realizando uma requisição para a criação de produto
+        const req = { body: {} }
+        const produto = await ProdutoControler.criar(req);
+        // Analisamos a const produto para verificar os dados retornados
+        expect(produto).toHaveProperty('cod_prod');
+        expect(produto.nome).toBe('Feijão');
+        expect(produto.preco).toBe(4.90);
+    });
+
+
+
 })
