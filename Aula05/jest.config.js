0c6a30d71a46d53d11b00cb4007fec61e458e1a5
Joel Santos <i3975@rn.senac.local>
2025-07-15 17:05:10 -0300

    feat: Inicio Aula 07 - Testes de integração

--- Aula05/jest.config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();  // Carrega o arquivo .env.test para os testes
-
-module.exports = {
-  testEnvironment: 'node',
-  setupFiles: ['dotenv/config'],
-};
--- Aula05/src/modules/produto/tests/produtoController.test.js
+++ Aula05/src/modules/produto/tests/produtoController.test.js
@@ -5,18 +5,16 @@ const {sequelize} = require('../../../config/configDB');
 beforeAll( async () =>{
     await sequelize.sync({force: true})
 })
-
 afterAll(async ()=>{
     await sequelize.close();
 })
-
 describe('Testes do Produto Controller', ()=>{
     test('Deve criar um produto no banco de dados', async ()=>{
         //Aqui estamos realizando uma requisição para a criação de produto
         const produto = await ProdutoControler.criar({nome: 'Feijão', preco: 4.90, estoque:500});
         // Analisamos a const produto para verificar os dados retornados
         expect(produto).toHaveProperty('cod_prod');
-        //expect(produto.nome).toBe('Feijão');
-       // expect(produto.preco).toBe(4.90);
+        expect(produto.nome).toBe('Feijão');
+       expect(produto.preco).toBe(4.90);
     });
 })
