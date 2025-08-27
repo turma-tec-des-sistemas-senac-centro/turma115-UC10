c6ba934c016421d961f72bd9ffd271125dd64492
SERGIO CUNHA LIMA BEZERRA <a92345826@rn.senac.local>
2025-08-27 17:15:20 -0300

    testes back/front

--- Aula12/back/index.js
+++ Aula12/back/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { sequelize } = require('./src/config/configDB');
+const swaggerUi = require('swagger-ui-espress');
+const swaggerDocument = require('./ApiLivros.openapi.json');
+const cors =  require('cors')
+
+
 require('dotenv').config();
 const livroRoutes = require('./src/modules/livro/routes/livroRoutes');
 
 const app = express();
 const PORT = process.env.PORT
+app.use(cors());
 app.use(express.json());
 app.use('/livros', livroRoutes);
 
